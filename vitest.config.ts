cb546b24e27aad70886fb0fcb3632178b5067444
Jeff Man <jeffman231@hotmail.com>
2025-05-09 20:42:27 +0100

    Add Codecov bundler plugin

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,7 @@
 import { URL, fileURLToPath } from 'node:url';
 import { defineConfig } from 'vitest/config';
 import env from 'vite-plugin-env-compatible';
+import { codecovVitePlugin } from '@codecov/vite-plugin';
 
 export default defineConfig({
   test: {
@@ -9,7 +10,14 @@ export default defineConfig({
       reportsDirectory: './tests/coverage',
     },
   },
-  plugins: [env()],
+  plugins: [
+    env(),
+    codecovVitePlugin({
+      enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+      bundleName: 'visa-calculator',
+      uploadToken: process.env.CODECOV_TOKEN,
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./', import.meta.url)),
