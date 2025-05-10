b00bd593e229ae08fb14eb4da346a91f3d61be6f
Jeff Man <jeffman231@hotmail.com>
2025-05-10 23:54:20 +0100

    remove mock infrastructure

--- src/infrastructure/services/crash-reporter.service.mock.ts
+++
@@ -1,7 +0,0 @@
-import { ICrashReporterService } from '@/src/application/services/crash-reporter.service.interface';
-
-export class MockCrashReporterService implements ICrashReporterService {
-  report(_: any): string {
-    return 'errorId';
-  }
-}
